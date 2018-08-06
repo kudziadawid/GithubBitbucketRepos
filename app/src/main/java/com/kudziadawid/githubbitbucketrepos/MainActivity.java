e111fd79f041e2453565af18662524e0e4832b39
Dai <kudzia.dawid@gmail.com>
2018-08-06 13:12:54 +0200

    First init

---
+++ app/src/main/java/com/kudziadawid/githubbitbucketrepos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kudziadawid.githubbitbucketrepos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
